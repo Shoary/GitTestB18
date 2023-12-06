9602046178167c2441b3832da767a1e80e07ecc8
Shoary <anesshoary@gmail.com>
2023-12-05 20:25:27 -0600

    seconed commit

--- src/headlights.java
+++ src/headlights.java
@@ -2,5 +2,8 @@ public class headlights {
 
     public static void main(String[] args) {
 
+        System.out.println("hello batch18");    System.out.println("hello batch18");
+        System.out.println("hello batch18");    System.out.println("hello batch18");
+
     }
 }
